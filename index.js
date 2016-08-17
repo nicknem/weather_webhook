cf02c68c45183ada20bf3393060b3a2f65228681
Antonin Archer <antonin.archer@hec.edu>
2016-08-17 09:28:17 +0200

    Add Procfile

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
+var openweatherKey = process.env.OPENWEATHER_KEY;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -8,3 +10,14 @@ app.get('/', function (req, res) {
 app.listen(3000, function() {
   console.log("Listening on port 3000!");
 });
+
+// Setup a server with a controller to listen to the webhook you set up on Motion AI
+// Call the OpenWeatherMap API
+// Respond to the webhook with whatever info you want using the response from OpenWeatherMap
+// Finally use Custom Variables to display info to the user
+
+// It gets location from Motion.AI
+
+var getCurrentWeather = function(uri, location) {
+  // Call weather API
+};
